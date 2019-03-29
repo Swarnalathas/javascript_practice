ec814b74335ea5b404da4cc3cceba42f9fbd7285
Swarna <swarna.prakashm@gmail.com>
2019-03-29 23:08:55 +0000

    Week2 task

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,26 +1,32 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;  
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.city === "Manchester" ;
 }
 
 function getBusNumbers(people) {
-  if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  if (people === undefined) throw new Error("people is required");  
+  return people % 40 === 0 ? Number.parseInt(people / 40) : (Number.parseInt(people/40) + 1);
 }
 
 function countSheep(arr) {
   if (!arr) throw new Error("arr is required");
-  // Your code here!
+  let count = 0;
+  arr.forEach(element => {    
+     if(element === "sheep") {
+       count++;
+     }     
+  });
+  return count;
 }
 
 function hasMPostCode(person) {
-  if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person === undefined) throw new Error("person is required");  
+  return person.address.postCode[0] === "M" && person.address.city === "Manchester";
 }
 
 module.exports = {
