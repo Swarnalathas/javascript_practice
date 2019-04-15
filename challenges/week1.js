349fa83ab9a7d43a148748ecbd0463729603b4ab
Swarna <swarna.prakashm@gmail.com>
2019-04-15 19:05:02 +0100

    Week4 Task completed

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,48 +1,45 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   var firstCapLetter = word.charAt(0).toUpperCase() + word.slice(1);
-  return firstCapLetter;  
+  return firstCapLetter;
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   var genarateInitials = firstName.charAt(0).toUpperCase() + "." + lastName.charAt(0).toUpperCase();
- return genarateInitials;
-
+  return genarateInitials;
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  const addVatToTotal =  (originalPrice /100 ) * vatRate ;
+  const addVatToTotal = (originalPrice / 100) * vatRate;
   const totalAmt = originalPrice + addVatToTotal;
-  return Number.isInteger(totalAmt)? totalAmt : +totalAmt.toFixed(2);
-  
+  return Number.isInteger(totalAmt) ? totalAmt : +totalAmt.toFixed(2);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  const reducePrice =  (originalPrice/100) * reduction;
+  const reducePrice = (originalPrice / 100) * reduction;
   const totalAmtReduce = originalPrice - reducePrice;
-  return Number.isInteger(totalAmtReduce)? totalAmtReduce : +totalAmtReduce.toFixed(2);
+  return Number.isInteger(totalAmtReduce) ? totalAmtReduce : +totalAmtReduce.toFixed(2);
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   var checkMidString = str.length / 2;
-  if (str.length % 2  === 0){
-    return (str.slice(checkMidString-1, checkMidString+1));
-  } else{
+  if (str.length % 2 === 0) {
+    return (str.slice(checkMidString - 1, checkMidString + 1));
+  } else {
     return str.slice(checkMidString, checkMidString + 1);
-  }  
+  }
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   return word.split("").reverse().join('');
-   
 }
 
 function reverseAllWords(words) {
@@ -51,15 +48,14 @@ function reverseAllWords(words) {
   words.forEach(element => {
     reverseWords.push(reverseWord(element));
   });
-  
   return reverseWords;
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-   return users.filter(element => {
-              return (element.type === 'Linux');
-          }).length;
+  return users.filter(element => {
+    return (element.type === 'Linux');
+  }).length;
 }
 
 function getMeanScore(scores) {
@@ -68,14 +64,13 @@ function getMeanScore(scores) {
   scores.forEach(element => {
     totalScores = totalScores + element;
   });
-  var meanScore = totalScores/scores.length;
-
-  return Number.isInteger(meanScore)? meanScore : +meanScore.toFixed(2);
+  var meanScore = totalScores / scores.length;
+  return Number.isInteger(meanScore) ? meanScore : +meanScore.toFixed(2);
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  if ((n % 3 === 0) && (n % 5 === 0)){
+  if ((n % 3 === 0) && (n % 5 === 0)) {
     return "fizzbuzz";
   } else if (n % 3 === 0) {
     return "fizz";
--- challenges/week4.js
+++ challenges/week4.js
@@ -1,43 +1,100 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let numLessOne = [];
+  nums.forEach(element => {
+    if (element < 1) {
+      numLessOne.push(element);
+    }
+  });
+  return numLessOne;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let namesBeginChar = [];
+  names.forEach(element => {
+    if (char === element[0]) {
+      namesBeginChar.push(element);
+    }
+  });
+  return namesBeginChar;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  let findVerbs = [];
+  words.forEach(element => {
+    let wordSplit = element.split(" ");
+    if (wordSplit[0] === 'to') {
+      findVerbs.push(wordSplit.join(' '));
+    }
+  });
+  return findVerbs;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let checkNumInt = [];
+  nums.forEach(element => {
+    if (Number.isInteger(element)) {
+      checkNumInt.push(element);
+    }
+  });
+  return checkNumInt;
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  let getUserName = [];
+  users.forEach(element => {
+    let cityName = element.data.city.displayName;
+    getUserName.push(cityName);
+  });
+  return getUserName;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let sqRootEachNum = [];
+  nums.forEach(element => {
+    let result = Math.sqrt(element);
+    if (Number.isInteger(result)) {
+      sqRootEachNum.push(result);
+    } else {
+      result = +result.toFixed(2);
+      sqRootEachNum.push(result);
+    }
+  });
+  return sqRootEachNum;
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  let searchSentences = [];
+  sentences.forEach(element => {
+    let regexp = new RegExp(str, 'i');
+    if (regexp.test(element)) {
+      searchSentences.push(element);
+    }
+  });
+  return searchSentences;
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  let longestSides = [];
+  triangles.forEach(element1 => {
+    let greaterNum = 0;
+    element1.forEach(element2 => {
+      if (element2 > greaterNum) {
+        greaterNum = element2;
+      }
+    });
+    longestSides.push(greaterNum);
+  });
+  return longestSides;
 }
 
 module.exports = {
